d94550f0616d08061f780091b154c56fc21708ba
Sohom Mondal <sohommondalofficial@gmail.com>
2025-12-12 12:58:07 +0530

    feat: Initialize project configuration and add sample Java program
    - Added .idea configuration files including .gitignore, IntelliLang.xml, OOP_LAB.iml, misc.xml, modules.xml, and vcs.xml.
    - Created a sample Java program (p1.java) to print user details including name, roll number, section, and branch.

---
+++ LAB_2/p1.java
@@ -0,0 +1,24 @@
+// Aim of the program -Write a program to print your name, roll no, section and branch in
+// separate lines.
+
+// Input: Mention Name,Roll no,Section and branch
+// Output: Display Name
+// Display Roll No
+// Display Section
+// Display Branch
+
+public class p1 {
+    public static void main(String[] args) {
+        // Declaring variables
+        String name = "Sohom Mondal";
+        int rollNo = 2405234;
+        String section = "CSE 33";
+        String branch = "Computer Science & Engineering";
+
+        // Printing details in separate lines
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
